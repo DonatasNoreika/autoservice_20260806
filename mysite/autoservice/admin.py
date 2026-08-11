9d978c479e0889564989f3cad16bf147d028efe0
DonatasNoreika <donoras@gmail.com>
2026-08-11 08:09:25 +0300

    3: +Į Automobilių sąrašą įdėti filtrą pagal klientą, automobilio gamintoją ir modelį

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -13,6 +13,7 @@ class OrderAdmin(admin.ModelAdmin):
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ['make', 'model', 'license_plate', 'vin_code', 'client_name']
+    list_filter = ['client_name', 'make', 'model']
 
 
 class ServiceAdmin(admin.ModelAdmin):
