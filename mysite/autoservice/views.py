f7f898ed601f90a04d080f4f8835cd35418e81c6
DonatasNoreika <donoras@gmail.com>
2026-08-17 08:20:25 +0300

    6: +Puslapyje įdėti paieškos laukelį, kuris ieškotų automobilių pagal savininką, markę, modelį, valstybinį numerį, VIN kodą

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Service, Car, Order
 from django.views import generic
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -41,3 +42,16 @@ class OrderDetailView(generic.DetailView):
     model = Order
     template_name = "order.html"
     context_object_name = "order"
+
+
+def search(request):
+    query = request.GET.get('query')
+    context = {
+        "query": query,
+        "cars": Car.objects.filter(Q(make__icontains=query) |
+                                   Q(model__icontains=query) |
+                                   Q(license_plate__icontains=query) |
+                                   Q(vin_code__icontains=query) |
+                                   Q(client_name__icontains=query))
+    }
+    return render(request, template_name="search.html", context=context)
