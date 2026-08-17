ca011a6e300e73f01a5dc3fdacbdfc93f0e94d60
DonatasNoreika <donoras@gmail.com>
2026-08-17 08:05:16 +0300

    6: +Padaryti, kad įrašai užsakymų puslapyje būtų puslapiuojami (per klasę).

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -29,6 +29,7 @@ class OrderListView(generic.ListView):
     model = Order
     template_name = "orders.html"
     context_object_name = "orders"
+    paginate_by = 2
 
 
 class OrderDetailView(generic.DetailView):
