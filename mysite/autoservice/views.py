ca39e31ece7cd5e3bb1b1eb236c423bdeffa7fb9
DonatasNoreika <donoras@gmail.com>
2026-08-18 08:02:48 +0300

    7: +Padaryti, kad pagrindiniame puslapyje būtų rodomas vartotojo apsilankymų skaičius.

--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -6,10 +6,13 @@ from django.db.models import Q
 
 # Create your views here.
 def index(request):
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_cars': Car.objects.count(),
         'num_services': Service.objects.count(),
         'num_orders_done': Order.objects.filter(status='i').count(),
+        'num_visits': num_visits,
     }
     return render(request, template_name="index.html", context=context)
 
