f9e417d44c3097dc0310ac8a5f45a28ab981b263
DonatasNoreika <donoras@gmail.com>
2026-08-24 08:15:55 +0300

    10: +Sukurti naują profilio puslapį, kuriame galima redaguoti vartotojo informaciją (el. paštą, vardą, pavardę), pakeisti nuotrauką.

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("search/", views.search, name="search"),
     path('userorders/', views.UserOrderListView.as_view(), name="user_orders"),
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path("profile/", views.profile, name="profile"),
 ]
--- mysite/autoservice/views.py
+++ mysite/autoservice/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render, reverse
+from django.shortcuts import render, reverse, redirect
 from django.views.generic.edit import FormMixin
 from .models import Service, Car, Order
 from django.views import generic
@@ -7,7 +7,8 @@ from django.core.paginator import Paginator
 from django.db.models import Q
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from .forms import OrderCommentForm
+from .forms import OrderCommentForm, UserChangeForm, ProfileChangeForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -96,3 +97,19 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     template_name = "signup.html"
     success_url = reverse_lazy("login")
+
+
+@login_required
+def profile(request):
+    u_form = UserChangeForm(request.POST or None, instance=request.user)
+    p_form = ProfileChangeForm(request.POST or None, request.FILES, instance=request.user.profile)
+    if u_form.is_valid() and p_form.is_valid():
+        u_form.save()
+        p_form.save()
+        return redirect("profile")
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, template_name="profile.html", context=context)
